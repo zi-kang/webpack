e772d42682217961f33312ab8b3ec6d63c6fb9fe
huzikang <huzikang@sightp.com>
2017-06-01 18:05:32 +0800

    增加插件的学习

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 /**
  * Created by huzikang on 17/6/1.
  */
-var path  = require('path');
+// var  path  = require('path');
 
 /*module.exports = {
   entry: './test/3.js',
@@ -10,7 +10,7 @@ var path  = require('path');
         filename: 'my-first-webpack.bundle.js'
     }
 };*/
-var config = {
+/*var config = {
     entry: {
         app: './test/123.js',
             search: './test/23.js'
@@ -19,7 +19,46 @@ var config = {
         filename: '[name].js',
             path: __dirname + '/build'
     }
+};*/
+
+
+/*const config = {
+    entry: './test/23.js',
+    output: {
+        path: path.resolve(__dirname, 'dist1'),
+        filename: 'my-first-webpack.bundle.js'
+    },
+    module: {
+        rules: [
+            {test: /\.(js|jsx)$/, use: 'babel-loader'}
+        ]
+    }
+};*/
+
+const path = require('path'),
+    webpack = require('webpack'),
+    HtmlWebpackPlugin = require('html-webpack-plugin');
+const config = {
+    entry: './test/3.js',
+    output: {
+        path: path.resolve(__dirname, 'dist2'),
+        filename: 'my-first-webpack.bundle.js'
+    },
+    module: {
+        rules: [
+            {test: /\.(js|jsx)$/, use: 'babel-loader'}
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new HtmlWebpackPlugin({template: './index.html'})
+    ]
 };
+
+
+
+
+
 module.exports = config;
 
 

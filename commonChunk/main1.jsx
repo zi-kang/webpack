6ab3a8d4400567e4dc3fb17093232e6ff1801b55
huzikang <huzikang@sightp.com>
2017-06-14 17:04:06 +0800

    add common chunk code

---
+++ commonChunk/main1.jsx
@@ -0,0 +1,10 @@
+/**
+ * Created by huzikang on 17/6/14.
+ */
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+<h1>Hello World</h1>,
+    document.getElementById('a')
+);
